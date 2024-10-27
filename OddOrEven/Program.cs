118b995932c56fc4a2f4b1c5dff0fe7e40804a5b
Grex <gregor.kaljumae@tktk.ee>
2024-10-27 22:47:13 +0200

    add week 3 code

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int resulte = userNum % 2;
+
+if(resulte != 0) //!= - ei ole 
+{
+    Console.WriteLine("User number is odd.");
+} else 
+{ 
+    Console.WriteLine("User number is even.");
+}
